0f99c642ff2268c699a654ed5e7cf36363210a1e
wkv816 <wkv816@haw-hamburg.de>
2023-10-25 16:44:43 +0200

    created player and interface

--- Praktikum_1/src/Server/Server.java
+++ Praktikum_1/src/Server/Server.java
@@ -1,4 +1,26 @@
 package Server;
 
-public class Server {
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Server implements TicTacToeAService {
+    private int game_ID;
+    private String opponent_name;
+
+
+    @Override
+    public HashMap<String, String> findGame(String clientName) throws RemoteException {
+        return null;
+    }
+
+    @Override
+    public String makeMove(int x, int y, String gameId) throws RemoteException {
+        return null;
+    }
+
+    @Override
+    public ArrayList<String> fullUpdate(String gameId) throws RemoteException {
+        return null;
+    }
 }
